4c5483125fcd12e111cffe1a1cadf67b8777ff7e
Eugene Sharygin <eush77@gmail.com>
2015-05-17 19:07:59 +0300

    Improve regular expression's overlapping and details

--- src/nbsp-positions.js
+++ src/nbsp-positions.js
@@ -4,7 +4,11 @@ var execall = require('regexp.execall');
 
 
 module.exports = function (text) {
-  return execall(/ \w{1,2} [^ ]/g, text).map(function (match) {
-    return match.index + match[0].length - 2;
-  });
+  return execall(/(^|\s\W*)(\w{1,2})(?= [^ ])/g, text)
+    .filter(function (match) {
+      return !/\d/.test(match[1]);
+    })
+    .map(function (match) {
+      return match.index + match[0].length;
+    });
 };
