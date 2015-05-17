8743b0297bd1e0fc0c8b7fb256d910d80063fde1
Eugene Sharygin <eush77@gmail.com>
2015-05-17 03:48:53 +0300

    Skip groups of more than one space

--- src/nbsp-positions.js
+++ src/nbsp-positions.js
@@ -4,7 +4,7 @@ var execall = require('regexp.execall');
 
 
 module.exports = function (text) {
-  return execall(/ \w{1,2} /g, text).map(function (match) {
-    return match.index + match[0].length - 1;
+  return execall(/ \w{1,2} [^ ]/g, text).map(function (match) {
+    return match.index + match[0].length - 2;
   });
 };
